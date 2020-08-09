4494daa080fe588cce678e45141bbe7d4d6b31f0
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-08-09 12:44:02 +0200

    Use Function clauses for route Handler

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,35 +4,51 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> log
     |> route
     |> format_response
   end
 
+  def log(conv), do: IO.inspect conv
+
   def parse(request) do
-    # TODO: Parse the request string into a map
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "" }
+    [method, path, _version] =
+      request
+      |> String.split("\n")
+      |> List.first
+      |> String.split
+
+    %{ method: method, path: path, resp_body: "" }
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+    # Map.put(conv, :resp:body, "Bears, Lions, Tigers")
+    # %{ conv | resp_body: "<h1 class=\"large\">Lions, Bears, and Tigers</h1>" }
+    route(conv, conv.path)
+  end
+
+  def route(conv, "/wildthings") do
+    %{ conv | resp_body: "<h1 class=\"large\">Lions, Bears, and Tigers</h1>" }
+  end
+
+  def route(conv, "/bears") do
+    %{ conv | resp_body: "<h1 class=\"large\">Bears</h1>" }
   end
 
   def format_response(conv) do
-    # TODO: User values in the map to create an HTTP response string:
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{byte_size(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
 
 
 # Request
-# Request line: HTTP Verb, HTTP URI and HTTP Version
+# Request line: HTTP Verb, HTTP URI and HTTP Protocol
 # Headers line
 # UserAgent line
 # Accept line
@@ -46,18 +62,27 @@ Accept: */*
 """
 
 # HTTP Response
-# Status Line: HTTP version, Status Code, Reason Phrase
+# Status Line: HTTP Protocol, Status Code, Reason Phrase
 # Response Headers: Media Type and Size of the Body
 # Blank line (sep headers from body)
 # Body
 expected_response = """
 HTTP/1.1 200 OK
 Content-Type: text/html
-Content-Length: 20
+Content-Length: 47
 
-Bears, Lions, Tigers
-
+<h1 class="large">Lions, Bears, and Tigers</h1>
 """
 
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
 response = Servy.Handler.handle(request)
 IO.puts(response)
