0801e06dcb8f23b33615236fbb80c3f7cc2b783c
Rui Freitas <dinnerpartysaboteur@gmail.com>
2020-08-09 13:13:59 +0200

    Refactor route Handler

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -4,10 +4,25 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
     |> log
     |> route
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "WARNING: #{path} is on the loose!!!!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{ path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(conv), do: conv
 
   def log(conv), do: IO.inspect conv
 
@@ -18,35 +33,56 @@ defmodule Servy.Handler do
       |> List.first
       |> String.split
 
-    %{ method: method, path: path, status: nil, resp_body: "" }
+    %{
+      method: method,
+      path: path,
+      status: nil,
+      resp_body: ""
+    }
   end
 
-  def route(conv) do
-    # Map.put(conv, :resp:body, "Bears, Lions, Tigers")
-    # %{ conv | resp_body: "<h1 class=\"large\">Lions, Bears, and Tigers</h1>" }
-    route(conv, conv.method, conv.path)
-  end
+  # def route(conv) do
+  #   # Map.put(conv, :resp:body, "Bears, Lions, Tigers")
+  #   # %{ conv | resp_body: "<h1 class=\"large\">Lions, Bears, and Tigers</h1>" }
+  #   route conv, conv.method, conv.path
+  # end
 
-  def route(conv, "GET", "/wildthings") do
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
     %{ conv | status: 200, resp_body: "<h1 class=\"large\">Lions, Bears, and Tigers</h1>" }
   end
 
-  def route(conv, "GET", "/bears") do
+  def route(%{ method: "GET", path: "/bears" } = conv) do
     %{ conv | status: 200, resp_body: "<h1 class=\"large\">Bears</h1>" }
   end
 
-  def route(conv, "GET", _) do
-    %{ conv | status: 404, resp_body: "404 - Not found" }
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "<h1 class=\"large\">Bear #{id}</h1>" }
+  end
+
+  # Catch all route
+  def route(%{ method: method, path: path } = conv) do
+    %{ conv | status: 404, resp_body: "404 - Cound not find #{method} request for #{path}" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 #{conv.status} OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -93,6 +129,26 @@ IO.puts(response)
 
 request = """
 GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+GET /wildlife HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
